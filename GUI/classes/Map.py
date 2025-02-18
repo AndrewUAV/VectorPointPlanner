109a97c4b313127a46ed2a00b2cba58917b6d1be
AndrewUAV <145654146+AndrewUAV@users.noreply.github.com>
2025-02-18 23:49:59 +0200

    add google map

--- GUI/classes/Map.py
+++ GUI/classes/Map.py
@@ -1,141 +1,44 @@
-import random
 import flet as ft
 import flet.map as map
 
 
-def main(page: ft.Page):
-    marker_layer_ref = ft.Ref[map.MarkerLayer]()
-    circle_layer_ref = ft.Ref[map.CircleLayer]()
+class Map:
+    def __init__(self, start_lat=None, start_lon=None, start_zoom=None,
+                 max_zoom=None, min_zoom=None, width=None, height=None):
+        self.expend = True
+        self.start_lat = 0 if start_lat is None else start_lat
+        self.start_lon = 0 if start_lon is None else start_lon
+        self.start_zoom = 5 if start_zoom is None else start_zoom
+        self.max_zoom = 15 if max_zoom is None else max_zoom
+        self.min_zoom = 2 if min_zoom is None else min_zoom
+        self.map_url = "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        self.width = 800 if width is None else width
+        self.height = 600 if height is None else height
 
-    def handle_tap(e: map.MapTapEvent):
-        print(e)
-        if e.name == "tap":
-            marker_layer_ref.current.markers.append(
-                map.Marker(
-                    content=ft.Icon(
-                        ft.Icons.LOCATION_ON, color=ft.cupertino_colors.DESTRUCTIVE_RED
-                    ),
-                    coordinates=e.coordinates,
-                )
-            )
-        elif e.name == "secondary_tap":
-            circle_layer_ref.current.circles.append(
-                map.CircleMarker(
-                    radius=random.randint(5, 10),
-                    coordinates=e.coordinates,
-                    color=ft.Colors.random_color(),
-                    border_color=ft.Colors.random_color(),
-                    border_stroke_width=4,
-                )
-            )
-        page.update()
 
-    def handle_event(e: map.MapEvent):
-        print(e)
-
-    page.add(
-        ft.Text("Click anywhere to add a Marker, right-click to add a CircleMarker."),
-        map.Map(
-            expand=True,
-            initial_center=map.MapLatitudeLongitude(15, 10),
-            initial_zoom=4.2,
-            interaction_configuration=map.MapInteractionConfiguration(
-                flags=map.MapInteractiveFlag.ALL
-            ),
-            on_init=lambda e: print(f"Initialized Map"),
-            on_tap=handle_tap,
-            on_secondary_tap=handle_tap,
-            on_long_press=handle_tap,
-            on_event=lambda e: print(e),
+    def setup_map(self):
+        return map.Map(
+            expand=self.expend,
+            initial_center=map.MapLatitudeLongitude(self.start_lat, self.start_lon),
+            initial_zoom=self.start_zoom,
+            max_zoom=self.max_zoom,
+            min_zoom=self.min_zoom,
+            width=self.width,
+            height=self.height,
             layers=[
                 map.TileLayer(
-                    url_template="https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-                    on_image_error=lambda e: print("TileLayer Error"),
-                ),
-                map.RichAttribution(
-                    attributions=[
-                        map.TextSourceAttribution(
-                            text="OpenStreetMap Contributors",
-                            on_click=lambda e: e.page.launch_url(
-                                "https://openstreetmap.org/copyright"
-                            ),
-                        ),
-                        map.TextSourceAttribution(
-                            text="Flet",
-                            on_click=lambda e: e.page.launch_url("https://flet.dev"),
-                        ),
-                    ]
-                ),
-                map.SimpleAttribution(
-                    text="Flet",
-                    alignment=ft.alignment.top_right,
-                    on_click=lambda e: print("Clicked SimpleAttribution"),
-                ),
-                map.MarkerLayer(
-                    ref=marker_layer_ref,
-                    markers=[
-                        map.Marker(
-                            content=ft.Icon(ft.Icons.LOCATION_ON),
-                            coordinates=map.MapLatitudeLongitude(30, 15),
-                        ),
-                        map.Marker(
-                            content=ft.Icon(ft.Icons.LOCATION_ON),
-                            coordinates=map.MapLatitudeLongitude(10, 10),
-                        ),
-                        map.Marker(
-                            content=ft.Icon(ft.Icons.LOCATION_ON),
-                            coordinates=map.MapLatitudeLongitude(25, 45),
-                        ),
-                    ],
-                ),
-                map.CircleLayer(
-                    ref=circle_layer_ref,
-                    circles=[
-                        map.CircleMarker(
-                            radius=10,
-                            coordinates=map.MapLatitudeLongitude(16, 24),
-                            color=ft.Colors.RED,
-                            border_color=ft.Colors.BLUE,
-                            border_stroke_width=4,
-                        ),
-                    ],
-                ),
-                map.PolygonLayer(
-                    polygons=[
-                        map.PolygonMarker(
-                            label="Popular Touristic Area",
-                            label_text_style=ft.TextStyle(
-                                color=ft.Colors.BLACK,
-                                size=15,
-                                weight=ft.FontWeight.BOLD,
-                            ),
-                            color=ft.Colors.with_opacity(0.3, ft.Colors.BLUE),
-                            coordinates=[
-                                map.MapLatitudeLongitude(10, 10),
-                                map.MapLatitudeLongitude(30, 15),
-                                map.MapLatitudeLongitude(25, 45),
-                            ],
-                        ),
-                    ],
-                ),
-                map.PolylineLayer(
-                    polylines=[
-                        map.PolylineMarker(
-                            border_stroke_width=3,
-                            border_color=ft.Colors.RED,
-                            gradient_colors=[ft.Colors.BLACK, ft.Colors.BLACK],
-                            color=ft.Colors.with_opacity(0.6, ft.Colors.GREEN),
-                            coordinates=[
-                                map.MapLatitudeLongitude(10, 10),
-                                map.MapLatitudeLongitude(30, 15),
-                                map.MapLatitudeLongitude(25, 45),
-                            ],
-                        ),
-                    ],
-                ),
+                    url_template=self.map_url
+                )
             ],
-        ),
-    )
+        )
 
+    def create_map(self):
+        return ft.Container(
+            content=ft.Column(
+                controls=[
+                    self.setup_map()
+                ]
+            ),
+            alignment=ft.Alignment(1, 0),
+        )
 
-ft.app(main)
--- GUI/classes/VectorPointPlanner.py
+++ GUI/classes/VectorPointPlanner.py
@@ -1,6 +1,7 @@
 ''' The main window of GUI '''
 import flet as ft
 
+from Map import Map
 from styles.Page import Page
 from styles.Button import Button
 
@@ -8,11 +9,13 @@ from styles.Button import Button
 class VectorPointPlanner:
     def __init__(self):
         self.vector_point_planner_page = Page(_title='VectorPointPlanner')
+        self.map = Map()
 
         """ CREATE BUTTON """
-        self.button_data_creator = Button(text='Дані', on_click='data_click')
-        self.button_plan_creator = Button(text='Планування місії', width=150, on_click='mission_planning_click')
-        self.button_settings_creator = Button(text='Налаштування', width=150, on_click='settings_click')
+        self.button_data_creator = Button(text='Data', on_click='data_click')
+        self.button_plan_creator = Button(text='Plan mission', width=150, on_click='mission_planning_click')
+        self.button_settings_creator = Button(text='Settings', width=150, on_click='settings_click')
+
 
         """ GET BUTTON """
         self.button_data = self.button_data_creator.create_button()
@@ -27,7 +30,12 @@ class VectorPointPlanner:
     def get_page(self, page: ft.Page):
         self.vector_point_planner_page.setup_page(page)
 
+        """ ADD BUTTONS """
         main_buttons_row = self.vector_point_planner_page.add_buttons_like_row(list_buttons=[self.button_data,
                                                                                              self.button_plan,
                                                                                              self.button_settings])
         page.add(main_buttons_row)
+
+        """ ADD MAP """
+        page.add(self.map.create_map())
+
